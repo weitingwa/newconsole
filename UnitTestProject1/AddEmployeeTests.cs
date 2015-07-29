85204c85dc33b97e9510124524a5b654e77b5c3a
weitingwa <fu629464@gmail.com>
2015-07-29 10:28:35 +0800

    modify Unit test file
    Signed-off-by: weitingwa <fu629464@gmail.com>

--- UnitTestProject1/AddEmployeeTests.cs
+++ UnitTestProject1/AddEmployeeTests.cs
@@ -12,19 +12,19 @@ namespace EmployeeTests
         [TestMethod]
         public void Test_Add_Employee_age_19()
         {
-            //setup
             Employee Cindy = new Taiwaneese("Cindy", 19);
-            //execute
             Cindy.age = 19;
-            //assure
+
             Assert.AreEqual(Cindy.hire(), false);
         }
 
         [TestMethod]
         public void Test_Add_Employee_age_24()
         {
+ 
             Employee Cindy = new Taiwaneese("Cindy", 24);
             Cindy.age = 24;
+            
             Assert.AreEqual(Cindy.hire(), true);
         }
     }
